360bcbd4e7950422ff6f8f87661fa2ab30c14b54
Mini-Valeria <160169222+Mini-Valeria@users.noreply.github.com>
2026-03-27 17:17:03 -0600

    fix firebase auth for render

--- config/firebase.js
+++ config/firebase.js
@@ -1,6 +1,14 @@
 const admin = require('firebase-admin');
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+let serviceAccount;
+
+if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+  // RENDER
+  serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+} else {
+  // LOCAL
+  serviceAccount = require('./stackburger-a3c81-firebase-adminsdk-fbsvc-454a341e0c.json');
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
