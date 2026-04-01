bc2c21e8b6b844865ec9521fe23d72b88135c80f
Mini-Valeria <160169222+Mini-Valeria@users.noreply.github.com>
2026-04-01 00:03:21 -0600

    Railway

--- config/firebase.js
+++ config/firebase.js
@@ -1,26 +1,11 @@
 const admin = require('firebase-admin');
+const serviceAccount = require('./firebase-service-account.json');
 
-if (!admin.apps.length) {
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 
-  let serviceAccount;
-
-  if (process.env.FIREBASE_PRIVATE_KEY) {
-    serviceAccount = {
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY.split('\\n').join('\n')
-    };
-    console.log("🔥 PRIVATE KEY START:", process.env.FIREBASE_PRIVATE_KEY?.slice(0, 30));
-  } else {
-    serviceAccount = require('./stackburger-a3c81-firebase-adminsdk-fbsvc-454a341e0c.json');
-  }
-
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
-  });
-
-  console.log("🔥 Firebase initialized correctly");
-}
+console.log("🔥 Firebase conectado correctamente");
 
 const db = admin.firestore();
 
